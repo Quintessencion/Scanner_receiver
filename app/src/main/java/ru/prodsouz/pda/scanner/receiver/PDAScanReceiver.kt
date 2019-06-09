b716458dedede30e84e4b3e5130d70bd6d77059b
quintessencion <zzerrorzz@list.ru>
2019-06-09 20:34:36 +0400

    refactoring

--- app/src/main/java/ru/prodsouz/pda/scanner/receiver/PDAScanReceiver.kt
+++ app/src/main/java/ru/prodsouz/pda/scanner/receiver/PDAScanReceiver.kt
@@ -8,48 +8,16 @@ import ru.prodsouz.pda.scanner.repository.SharedPrefs
 
 class PDAScanReceiver : BroadcastReceiver() {
 
-    companion object {
-        const val SCAN_ACTION = "scan.rcv.enter"
-        const val SCAN_EXTRA_KEY = "codestr"
-
-//        const val ACTION_SEND_INTENT = "ru.prodsouz.pda.scanner.barcode.intent"
-//        const val BARCODE_DATA = "BARCODE_DATA"
-    }
-
     override fun onReceive(context: Context, intent: Intent) {
         when (intent.action) {
-            SCAN_ACTION -> {
-                val code = intent.extras?.getString(SCAN_EXTRA_KEY) ?: return
+            BuildConfig.SCAN_ACTION -> {
+                val code = intent.extras?.getString(BuildConfig.SCAN_EXTRA_KEY) ?: return
                 SharedPrefs.sharedPreferences =
                     context.getSharedPreferences(BuildConfig.APPLICATION_ID, Context.MODE_PRIVATE)
                 SharedPrefs.barCode = code
             }
             else -> return
         }
-
-//        val data =
-//            when (intent.action) {
-//                SCAN_ACTION -> intent.extras?.getString(SCAN_EXTRA_KEY) ?: return
-//                else -> return
-//            }
-//
-//        var infoText = context.getString(R.string.send_broadcast)
-//
-//        val intentFor1C = Intent(ACTION_SEND_INTENT).apply { putExtra(BARCODE_DATA, data) }
-//        context.sendBroadcast(intentFor1C)
-//
-//        val activities: List<ResolveInfo> = context.packageManager.queryIntentActivities(
-//            intentFor1C,
-//            PackageManager.MATCH_DEFAULT_ONLY
-//        )
-//        if (activities.isNotEmpty()) {
-//            context.startActivity(intentFor1C)
-//            infoText += "\n\n" + context.getString(R.string.data_transfer_to_another_app)
-//        } else {
-//            infoText += "\n\n" + context.getString(R.string.no_intent_activity)
-//        }
-//
-//        Toast.makeText(context, infoText, Toast.LENGTH_SHORT).show()
 
         abortBroadcast()
     }
--- app/src/main/java/ru/prodsouz/pda/scanner/ui/DataActivity.kt
+++ app/src/main/java/ru/prodsouz/pda/scanner/ui/DataActivity.kt
@@ -2,7 +2,6 @@ package ru.prodsouz.pda.scanner.ui
 
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import ru.prodsouz.pda.scanner.BuildConfig
@@ -10,12 +9,16 @@ import ru.prodsouz.pda.scanner.repository.SharedPrefs
 
 class DataActivity : AppCompatActivity() {
 
+    companion object {
+        const val BARCODE_DATA = "BARCODE_DATA"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         SharedPrefs.sharedPreferences = getSharedPreferences(BuildConfig.APPLICATION_ID, Context.MODE_PRIVATE)
 
-        setResult(RESULT_OK, Intent().apply { putExtra("name", SharedPrefs.barCode) })
+        setResult(RESULT_OK, Intent().apply { putExtra(BARCODE_DATA, SharedPrefs.barCode) })
 
         finish()
     }
